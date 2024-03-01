6ec6d017b133a9d0181d485e2c2d40b5bf94b843
Andrew Thomas <andrew@andrewrthomas.com>
2024-03-01 14:22:41 -0800

    slowwwwwww

--- automata/src/editor.rs
+++ automata/src/editor.rs
@@ -36,15 +36,13 @@ pub(crate) fn create(
 
         VStack::new(cx, |cx| {
             Label::new(cx, "Automata")
-                .font_family(vec![FamilyOwned::Name(String::from(
-                    assets::NOTO_SANS_THIN,
-                ))])
+                .font_family(vec![FamilyOwned::Name(String::from(assets::NOTO_SANS))])
                 .font_size(30.0)
                 .height(Pixels(50.0))
                 .child_top(Stretch(1.0))
                 .child_bottom(Pixels(0.0));
             Button::new(cx, |_| {}, |cx| Label::new(cx, "start"));
-            Button::new(cx, |_| {}, |cx| Label::new(cx, "reset"))
+            Button::new(cx, |_| {}, |cx| Label::new(cx, "reset"));
         })
         .row_between(Pixels(0.0))
         .child_left(Stretch(1.0))
